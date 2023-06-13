7185071fec97d393d8a3c6208540eb7dee4f4bd5
Endale Yohannes <endaleyohannes8@gmail.com>
2023-06-13 09:47:21 +0300

    add link to signup page

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // output: "export",
+  output: "export",
   images: {
     domains: ['media.licdn.com','picsum.photos','res.cloudinary.com',"github.com"]
   },
