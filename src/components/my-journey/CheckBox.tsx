e6b49cb067d99e4054d3f15edb09d1ca889e0a4b
Zerihun Moges <zerihunmoges78@gmail.com>
2023-06-13 04:25:37 -0700

    implment journy ui

---
+++ src/components/my-journey/CheckBox.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { BiCheck } from "react-icons/bi";
+interface CheckBoxProps {
+  isCompleted: boolean;
+  stepper?: boolean;
+  active?: boolean;
+}
+const CheckBox: React.FC<CheckBoxProps> = ({
+  isCompleted,
+  stepper,
+  active,
+}) => {
+  return (
+    <div
+      className={`${
+        stepper ? "w-4 h-4" : "w-5 h-5"
+      } flex shrink-0 rounded-full items-center justify-center  ${
+        isCompleted
+          ? "bg-primary"
+          : active
+          ? "border-4 border-primary"
+          : "border-4"
+      }`}
+    >
+      {isCompleted && <BiCheck className="w-full h-full text-white" />}
+    </div>
+  );
+};
+
+export default CheckBox;
---
+++ src/components/my-journey/contestDetailTimer.tsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from "react";
+
+interface ITimeLeft {
+  days: string;
+  hours: string;
+  minutes: string;
+  seconds: string;
+}
+
+function getRemainingTime(targetDate: Date): ITimeLeft {
+  const currentDate = new Date();
+  const timeDifference = targetDate.getTime() - currentDate.getTime();
+  if (timeDifference < 0) {
+    return { days: "0", hours: "00", minutes: "00", seconds: "00" };
+  }
+  const remainingDays = Math.floor(timeDifference / (24 * 60 * 60 * 1000));
+
+  const remainingHours = Math.floor(timeDifference / (60 * 60 * 1000));
+  const remainingMinutes = Math.floor(
+    (timeDifference % (60 * 60 * 1000)) / (60 * 1000)
+  );
+  const remainingSeconds = Math.floor((timeDifference % (60 * 1000)) / 1000);
+
+  return {
+    days: `${remainingDays}`,
+    hours: `${String(remainingHours).padStart(2, "0")}`,
+    minutes: `${String(remainingMinutes).padStart(2, "0")}`,
+    seconds: `${String(remainingSeconds).padStart(2, "0")}`,
+  };
+}
+
+interface ContestDetailTimerProps {
+  date: Date;
+}
+
+const ContestDetailTimer: React.FC<ContestDetailTimerProps> = ({ date }) => {
+  const [remainingTime, setRemainingTime] = useState<ITimeLeft>();
+  useEffect(() => {
+    const timeLeft: ITimeLeft = getRemainingTime(date);
+    setRemainingTime(timeLeft);
+
+    const intervalId = setInterval(() => {
+      setRemainingTime(getRemainingTime(date));
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [date]);
+  return (
+    <div className="grid grid-cols-4 max-w-xs">
+      <div className="grid items-start p-2">
+        <p className="text-sm">Days</p>
+        <p className="text-3xl">{remainingTime?.days}</p>
+      </div>
+      <div className="grid items-start p-2">
+        <p className="text-sm">Hours</p>
+        <p className="text-3xl">{remainingTime?.hours}</p>
+      </div>
+      <div className="grid items-start p-2">
+        <p className="text-sm">Minutes</p>
+        <p className="text-3xl">{remainingTime?.minutes}</p>
+      </div>
+      <div className="grid items-start p-2">
+        <p className="text-sm">Seconds</p>
+        <p className="text-3xl">{remainingTime?.seconds}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ContestDetailTimer;
