9b811a32aee8c28666c510f1924c99be1348a70e
Endale Yohannes <endaleyohannes8@gmail.com>
2023-06-13 19:03:24 +0300

    change img format to svg

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
   images: {
     domains: ['media.licdn.com','picsum.photos','res.cloudinary.com',"github.com"]
   },
