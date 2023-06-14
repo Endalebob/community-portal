bd577205ba02c5403ce5f00a12ea33b0b750bcf8
mekdeskebede <kebedemekdes289@gmail.com>
2023-06-14 09:27:36 +0300

    resolve merge conflict

--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -7,13 +7,13 @@ const Landing: React.FC = () => {
     <div className="bg-primarybg font-poppins ">
       <div className="relative">
         <Image
-          src="/img/home/home1.jpg"
-          alt="landing-img"
+          src="/images/home/home1.jpg"
+          alt="landing-image"
           width={2000}
           height={1000}
-          className="brightness-50 h-96 md:h-full"
+          className="brightness-50"
         />
-        <div className="md:absolute bottom-0.5 left-16 text-primary-text md:text-primarybg w-1/1 md:w-1/2 text-lg lg:bottom-1/3 m-4 md:m-0">
+        <div className="md:absolute bottom-0.5 left-16 text-primary-text md:text-primarybg w-1/1 md:w-7/12 lg:w-1/2 text-lg lg:bottom-1/4 m-4 md:m-0">
           <div className="mb-8 md:m-0 m-4">
             <h1 className="text-4xl font-bold md:pt-0 pt-8">Who We Are</h1>
             <p className="mt-8">
@@ -26,8 +26,8 @@ const Landing: React.FC = () => {
             </p>
           </div>
 
-          <div className="flex flex-col sm:flex-row mt-4 mx-auto my-auto">
-            <button className="bg-primary px-16 md:px-28 py-2 rounded-md mr-12 hover:bg-blue-400 font-bold">
+          <div className="flex flex-row mt-4 mx-auto my-auto">
+            <button className="ml-4 md:ml-0 bg-primary px-4 md:px-16 lg:px-24 xl:px-28 py-2 rounded-md mr-12 hover:bg-blue-400 font-bold text-white">
               Join Us
             </button>
             <div className="flex flex-row">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,21 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Landing from '<@>/components/home/Landing'
-import Welcome from '<@>/components/home/Welcome'
-import HowToJoin from '<@>/components/home/HowToJoin'
-import Partners from '<@>/components/home/Partners'
-import GetStarted from '<@>/components/home/GetStarted'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Landing from "<@>/components/home/Landing";
+import Welcome from "<@>/components/home/Welcome";
+import HowToJoin from "<@>/components/home/HowToJoin";
+import Partners from "<@>/components/home/Partners";
+import GetStarted from "<@>/components/home/GetStarted";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className="flex flex-col bg-primarybg">
-      <Landing/>
-      <Welcome/>
-      <HowToJoin/>
-      <Partners/>
-      <GetStarted/>
+      <Landing />
+      <Welcome />
+      <HowToJoin />
+      <Partners />
+      <GetStarted />
     </main>
-  )
+  );
 }
