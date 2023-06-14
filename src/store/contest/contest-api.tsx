4563bf7265800357b242eb1450e7fcdb79e2f2e8
mekdeskebede <kebedemekdes289@gmail.com>
2023-06-14 16:07:09 +0300

    add form validation

--- src/store/contest/contest-api.tsx
+++ src/store/contest/contest-api.tsx
@@ -2,7 +2,17 @@ import Contest from "<@>/types/contest";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const contestsApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "/api" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "/api",
+
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as any).auth.token;
+      if (token) {
+        headers.set("authorization", `token ${token}`);
+      }
+      return headers;
+    },
+  }),
   endpoints: (builder) => ({
     createContest: builder.mutation<any, Partial<Contest>>({
       query: (contest) => ({
