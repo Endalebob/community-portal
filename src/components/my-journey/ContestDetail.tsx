a40396f8df183e8386a72664ad4194244536a301
Zerihun Moges <zerihunmoges78@gmail.com>
2023-06-16 04:34:21 -0700

    add modal

--- src/components/my-journey/ContestDetail.tsx
+++ src/components/my-journey/ContestDetail.tsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import ContestDetailTimer from "./contestDetailTimer";
 import Button from "../common/Button";
 import { BiLinkExternal } from "react-icons/bi";
+import { AiOutlineClose } from "react-icons/ai";
 
 interface ContestDetailProps {
   id: string | number;
+  setSelectedContest: (id: string | number | null) => void;
 }
-const ContestDetail: React.FC<ContestDetailProps> = ({ id }) => {
+const ContestDetail: React.FC<ContestDetailProps> = ({
+  id,
+  setSelectedContest,
+}) => {
   const contest = {
     title: "Round 3 Community Contest for on-boarding",
     date: "Jun 16, 2023",
@@ -16,7 +21,16 @@ const ContestDetail: React.FC<ContestDetailProps> = ({ id }) => {
   };
   return (
     <div className="flex flex-col items-start w-full gap-6 max-w-sm">
-      <p className="font-medium text-xl">{contest.title}</p>
+      <div
+        onClick={() => setSelectedContest(null)}
+        className="flex mt-1 self-end justify-center w-7 h-7 items-center hover:bg-secondary rounded-full border p-1 transition-opacity"
+      >
+        <AiOutlineClose className="w-6 h-6" />
+      </div>
+      <div className="flex items-start">
+        <p className="font-medium text-xl">{contest.title}</p>
+      </div>
+
       <div className="mx-auto">
         <ContestDetailTimer date={new Date(contest.date)} />
       </div>
--- src/store/index.tsx
+++ src/store/index.tsx
@@ -2,16 +2,24 @@ import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import userSlice from "./auth/user-slice";
 import { authApiSlice } from "./auth/auth-api";
 import authSlice from "./auth/auth-slice";
+import { contestApiSlice } from "./journey/contest-api";
+import { stepApiSlice } from "./journey/journey-api";
 
 export const store = configureStore({
   reducer: {
     auth: authSlice,
     user: userSlice,
+    [contestApiSlice.reducerPath]: contestApiSlice.reducer,
+    [stepApiSlice.reducerPath]: stepApiSlice.reducer,
     [authApiSlice.reducerPath]: authApiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(authApiSlice.middleware);
-  }
+    return getDefaultMiddleware().concat(
+      authApiSlice.middleware,
+      contestApiSlice.middleware,
+      stepApiSlice.middleware
+    );
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
