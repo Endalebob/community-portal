2623aa1e990606f348defac6907d31afca3f4ade
Endale Yohannes <95867374+Endalebob@users.noreply.github.com>
2023-06-16 10:33:07 +0300

    resolve merge conflict (#9)
    Co-authored-by: mekdeskebede <kebedemekdes289@gmail.com>

--- src/components/home/Welcome.tsx
+++ src/components/home/Welcome.tsx
@@ -3,9 +3,9 @@ import React from "react";
 
 const Welcome: React.FC = () => {
   return (
-    <div className="grid grid-cols-1 text-primary-text mt-16 font-poppins lg:grid-cols-2 ml-8 md:ml-28 md:mr-8">
+    <div className="grid grid-cols-1 text-primary-text mt-8 font-poppins lg:grid-cols-2 ml-8 mr-8 md:mr-16 md:ml-16 lg:ml-28 ">
       <div className="col-span-1 mt-0 md:mt-16">
-        <h1 className="text-3xl font-extrabold mt-0 md:mt-16">
+        <h1 className="text-3xl font-extrabold mt-0 lg:mt-16">
           Welcome to the A2SV Community
         </h1>
         <p className="text-secondary-text mt-4 mb-4 text-lg">
@@ -20,23 +20,23 @@ const Welcome: React.FC = () => {
           solvers. Small enough to be simple and quick.
         </p>
       </div>
-      <div className="grid grid-cols-1 ml-8 md:ml-16 mt-16 sm:grid-cols-2">
+      <div className="grid grid-cols-1 lg:ml-16 mt-16 sm:grid-cols-2 gap-4  mx-auto lg:w-auto">
         <div className="h-96">
           <Image
-            src="/img/home/welcome1.jpg"
+            src="/images/home/welcome1.jpg"
             alt="welcome first image"
-            width={250}
+            width={1000}
             height={1000}
-            className="h-96 rounded-lg brightness-75 object-cover"
+            className="h-96 rounded-lg brightness-75 object-cover w-80 lg:w-auto"
           />
         </div>
         <div className="mt-8">
           <Image
-            src="/img/home/welcome2.jpg"
+            src="/images/home/welcome2.jpg"
             alt="welcome first image"
-            width={250}
+            width={1000}
             height={1000}
-            className="h-96 rounded-lg brightness-75 hover:brightness-50 object-cover"
+            className="h-96 rounded-lg brightness-75 object-cover w-80 lg:w-auto"
           />
         </div>
       </div>
