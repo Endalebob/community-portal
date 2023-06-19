cb1c12bc40284b610748725d70425ff4054678c0
mekdeskebede <kebedemekdes289@gmail.com>
2023-06-19 09:16:45 +0300

    add animation to landing page

--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -1,9 +1,15 @@
+import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 import { AiOutlineRight } from "react-icons/ai";
 
 const Landing: React.FC = () => {
+  const router = useRouter();
+  const handleJoinUs = () => {
+    router.push("/auth/signup");
+  };
   return (
     <div className="bg-primarybg font-poppins ">
       <div className="relative">
@@ -16,23 +22,55 @@ const Landing: React.FC = () => {
         />
         <div className="md:absolute bottom-0.5 left-16 text-primary-text md:text-primarybg w-1/1 md:w-7/12 lg:w-1/2 text-lg lg:bottom-1/4 m-4 md:m-0">
           <div className="mb-8 md:m-0 m-4">
-            <h1 className="text-4xl font-bold md:pt-0 pt-8">Who We Are</h1>
-            <p className="mt-8">
+            <motion.h1
+              className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold md:pt-0 pt-8"
+              initial={{ y: -1000 }}
+              animate={{ y: 0 }}
+              transition={{
+                duration: 2,
+                type: "spring",
+              }}
+            >
+              Who We Are
+            </motion.h1>
+            <motion.p
+              className="mt-8 md:text-lg lg:text-xl"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 2 }}
+            >
               A2SV upskills high-potential university students, connects them
               with opportunities at top tech companies around the world, and
               creates digital solutions to urgent problems in their home
               countries. The program is free for students, making the
               opportunity available for youth who have talent but lack the means
               to use it.
-            </p>
+            </motion.p>
           </div>
 
-          <div className="flex flex-row mt-4 mx-auto my-auto">
-            <Link href={"/auth/signup"}>
-              <button className="ml-4 md:ml-0 bg-primary px-4 md:px-16 lg:px-24 xl:px-28 py-2 rounded-md mr-12 hover:bg-blue-400 font-bold text-white">
-                Join Us
-              </button>
-            </Link>
+          <motion.div
+            className="flex flex-row mt-4 mx-auto my-auto"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 3 }}
+          >
+            <motion.button
+              className="ml-4 md:ml-0 bg-primary px-2 sm:px-4 md:px-16 lg:px-24 xl:px-28 py-2 rounded-md mr-12 hover:bg-blue-400 font-bold text-white"
+              initial={{ scale: 0.9 }}
+              animate={{
+                scale: 1,
+                transition: {
+                  duration: 1,
+                  repeat: Infinity,
+                  repeatType: "reverse",
+                  ease: "easeInOut",
+                },
+              }}
+              onClick={handleJoinUs}
+            >
+              Join Us
+            </motion.button>
+
             <Link href={"#more"}>
               <div className="flex flex-row">
                 <button className="text-end font-bold">More Information</button>
@@ -41,7 +79,7 @@ const Landing: React.FC = () => {
                 </div>
               </div>
             </Link>
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
--- src/components/home/Welcome.tsx
+++ src/components/home/Welcome.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import Image from "next/image";
 import React from "react";
 
@@ -7,24 +8,32 @@ const Welcome: React.FC = () => {
       className="grid grid-cols-1 text-primary-text mt-8 font-poppins lg:grid-cols-2 ml-8 mr-8 md:mr-16 md:ml-16 lg:ml-28 "
       id="more"
     >
-      <div className="col-span-1 mt-0 md:mt-16">
-        <h1 className="text-3xl font-extrabold mt-0 lg:mt-16">
+      <motion.div
+        className="col-span-1 mt-0 md:mt-16"
+        initial={{ x: -100 }}
+        whileInView={{ x: 0, transition: { duration: 1 } }}
+      >
+        <h1 className="text-xl sm:text-2xl lg:text-3xl font-extrabold mt-0 lg:mt-16">
           Welcome to the A2SV Community
         </h1>
-        <p className="text-secondary-text mt-4 mb-4 text-lg">
+        <p className="text-secondary-text mt-4 mb-4 md:text-lg lg:text-xl">
           We are strategists, designers and developers. Innovators and problem
           solvers. Small enough to be simple and quick, but big enough to
           deliver the scope you want at the pace you need. Small enough to be
           simple and quick, but big enough to deliver the scope you want at the
           pace you need.
         </p>
-        <p className="text-secondary-text text-lg">
+        <p className="text-secondary-text mt-4 mb-4 text-base md:text-lg lg:text-xl">
           We are strategists, designers and developers. Innovators and problem
           solvers. Small enough to be simple and quick.
         </p>
-      </div>
+      </motion.div>
       <div className="grid grid-cols-1 lg:ml-16 mt-16 sm:grid-cols-2 gap-4  mx-auto lg:w-auto">
-        <div className="h-96">
+        <motion.div
+          className="h-96"
+          initial={{ y: -50 }}
+          whileInView={{ y: 0, transition: { duration: 1 } }}
+        >
           <Image
             src="/images/home/welcome1.jpg"
             alt="welcome first image"
@@ -32,8 +41,12 @@ const Welcome: React.FC = () => {
             height={1000}
             className="h-96 rounded-lg brightness-75 object-cover w-80 lg:w-auto"
           />
-        </div>
-        <div className="mt-8">
+        </motion.div>
+        <motion.div
+          className="mt-8"
+          initial={{ y: 50 }}
+          whileInView={{ y: 0, transition: { duration: 1 } }}
+        >
           <Image
             src="/images/home/welcome2.jpg"
             alt="welcome first image"
@@ -41,7 +54,7 @@ const Welcome: React.FC = () => {
             height={1000}
             className="h-96 rounded-lg brightness-75 object-cover w-80 lg:w-auto"
           />
-        </div>
+        </motion.div>
       </div>
     </div>
   );
