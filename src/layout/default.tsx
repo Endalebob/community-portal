590dcd186956953c36a66fe129d0a9c5ac0416f3
Endale Yohannes <endaleyohannes8@gmail.com>
2023-06-21 23:04:04 +0300

    react-hydration-error solved

--- src/layout/default.tsx
+++ src/layout/default.tsx
@@ -1,10 +1,16 @@
 import NavBar from "<@>/components/layout/NavBar";
-import { setToken } from "<@>/store/auth/auth-slice";
-import { useAppDispatch, useAppSelector } from "<@>/store/hooks";
 import React, { useEffect } from "react";
 import { ReactNode, Suspense } from "react";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
+  const [hasMounted, setHasMounted] = React.useState(false);
+  React.useEffect(() => {
+    setHasMounted(true);
+  }, []);
+  if (!hasMounted) {
+    return null;
+  }
+
   return (
     <div>
       <NavBar />
