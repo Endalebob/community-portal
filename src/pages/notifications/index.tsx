bd19fb1bbeebdbdf851b8b87b1750af32936c97a
aklile-yilma <aklileyilma@gmail.com>
2023-06-23 10:12:38 +0300

    feat(fetch): fetch notifications

--- src/pages/notifications/index.tsx
+++ src/pages/notifications/index.tsx
@@ -1,5 +1,5 @@
 import { useGetNotificationsQuery } from "<@>/store/notifications/notifications-api";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FaRegCalendarAlt } from "react-icons/fa";
 import { Notification } from "<@>/types/notifications/notifications";
 
@@ -8,24 +8,33 @@ const Notifications = () => {
 
   const [notifications, setNotifications] = useState<Notification[]>([]);
 
-  if (data) {
-    setNotifications(data.value);
-  }
+  useEffect(() => {
+    if (data) {
+      setNotifications(data.value);
+    }
+  }, []);
 
   if (isLoading) {
-    <div className="bg-white rounded-lg shadow-lg p-6 animate-pulse">
-      <h2 className="text-xl font-semibold mb-2 bg-gray-300 h-6 w-1/2 rounded"></h2>
-      <p className="text-gray-700 mb-4 bg-gray-300 h-4 w-3/4 rounded"></p>
-      <div className="flex items-center text-gray-600">
-        <div className="bg-gray-300 h-4 w-4 rounded-full mr-2"></div>
-        <span className="bg-gray-300 h-4 w-16 rounded"></span>
-      </div>
-    </div>;
+    {
+      Array(4).map((_item, index) => (
+        <div
+          className="bg-white rounded-lg shadow-lg p-6 animate-pulse"
+          key={index}
+        >
+          <h2 className="text-xl font-semibold mb-2 bg-gray-300 h-6 w-1/2 rounded"></h2>
+          <p className="text-gray-700 mb-4 bg-gray-300 h-4 w-3/4 rounded"></p>
+          <div className="flex items-center text-gray-600">
+            <div className="bg-gray-300 h-4 w-4 rounded-full mr-2"></div>
+            <span className="bg-gray-300 h-4 w-16 rounded"></span>
+          </div>
+        </div>
+      ));
+    }
   }
 
   if (isError) {
     <div className="justify-center items-center">
-      <p>coundn't fetch notifications</p>
+      <p>couldn't fetch notifications</p>
     </div>;
   }
 
@@ -34,17 +43,20 @@ const Notifications = () => {
       <h1 className="text-3xl font-bold text-gray-800 mb-10">Notifications</h1>
 
       {notifications ? (
-        notifications.map((_item, index) => (
-          <div key={index} className="bg-white rounded-lg shadow-lg p-6">
-            <h2 className="text-xl font-semibold mb-2">My Title</h2>
-            <p className="text-gray-700 mb-4">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae
-              eros ut nisi suscipit dignissim. Donec euismod purus vel sem
-              pulvinar, vel hendrerit est dictum.
-            </p>
+        notifications.map(({ title, description, date, isRead }, index) => (
+          <div
+            key={index}
+            className={
+              isRead
+                ? "bg-white rounded-lg shadow-lg p-6 border border-red-400"
+                : "bg-white rounded-lg shadow-lg p-6"
+            }
+          >
+            <h2 className="text-xl font-semibold mb-2">{title}</h2>
+            <p className="text-gray-700 mb-4">{description}</p>
             <div className="flex items-center text-gray-600">
               <FaRegCalendarAlt className="mr-2" />
-              <span>June 21, 2023</span>
+              <span>{date.toDateString()}</span>
             </div>
           </div>
         ))
