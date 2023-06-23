07a13a8116518486f24090e7c4c3846b5235969f
aklile-yilma <aklileyilma@gmail.com>
2023-06-23 10:38:09 +0300

    stick footer to bottom

--- src/layout/default.tsx
+++ src/layout/default.tsx
@@ -1,20 +1,29 @@
+import Footer from "<@>/components/layout/Footer";
 import NavBar from "<@>/components/layout/NavBar";
+import { setToken } from "<@>/store/auth/auth-slice";
+import { useAppDispatch, useAppSelector } from "<@>/store/hooks";
 import React, { useEffect } from "react";
 import { ReactNode, Suspense } from "react";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
-  const [hasMounted, setHasMounted] = React.useState(false);
-  React.useEffect(() => {
-    setHasMounted(true);
+  const dispatch = useAppDispatch();
+  const { isAuthenticated } = useAppSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      const item = localStorage.getItem("token");
+      const role = localStorage.getItem("role");
+      if (item) {
+        dispatch(setToken({ token: item, role: role, isAuthenticated: true }));
+      }
+    }
   }, []);
-  if (!hasMounted) {
-    return null;
-  }
 
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <NavBar />
       <div>{children}</div>
+      <Footer />
     </div>
   );
 };
