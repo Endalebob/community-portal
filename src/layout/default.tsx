f60c668905b44119486e27ac6b93672a867aca29
aklile-yilma <aklileyilma@gmail.com>
2023-06-23 10:40:46 +0300

    rebase

--- src/layout/default.tsx
+++ src/layout/default.tsx
@@ -1,23 +1,16 @@
 import Footer from "<@>/components/layout/Footer";
 import NavBar from "<@>/components/layout/NavBar";
-import { setToken } from "<@>/store/auth/auth-slice";
-import { useAppDispatch, useAppSelector } from "<@>/store/hooks";
 import React, { useEffect } from "react";
 import { ReactNode, Suspense } from "react";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
-  const dispatch = useAppDispatch();
-  const { isAuthenticated } = useAppSelector((state) => state.auth);
-
-  useEffect(() => {
-    if (!isAuthenticated) {
-      const item = localStorage.getItem("token");
-      const role = localStorage.getItem("role");
-      if (item) {
-        dispatch(setToken({ token: item, role: role, isAuthenticated: true }));
-      }
-    }
+  const [hasMounted, setHasMounted] = React.useState(false);
+  React.useEffect(() => {
+    setHasMounted(true);
   }, []);
+  if (!hasMounted) {
+    return null;
+  }
 
   return (
     <div className="min-h-screen flex flex-col">
