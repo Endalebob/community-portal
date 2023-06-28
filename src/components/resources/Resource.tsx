3bb3e53434876eaf3041ce139a770cbfb6b8608b
Endale Yohannes <endaleyohannes8@gmail.com>
2023-06-28 22:42:12 +0300

    edit and delete button for resource

--- src/components/resources/Resource.tsx
+++ src/components/resources/Resource.tsx
@@ -1,21 +1,50 @@
-import { useGetResourceByIdQuery } from "<@>/store/resource/resource-api";
+import { useDeleteResourceMutation, useDeleteTopicMutation, useGetResourceByIdQuery } from "<@>/store/resource/resource-api";
 import IResourceTopic from "<@>/types/resources/resourceListType";
 import IResource from "<@>/types/resources/resourcesType";
-import React, { useEffect, useState } from "react";
+import React, { use, useEffect, useState } from "react";
+import { createMarkup } from "../common/TextEditor";
+import { AiFillDelete, AiFillEdit } from "react-icons/ai";
+import EditResources from "./EditResource";
+import Modal from "../common/Modal";
+import ProgressIndicator from "../common/ProgressIndicator";
 
 interface ResourceProps {
   selectedChapter: number;
+  selectedResource: number;
   resources: IResourceTopic[];
 }
 
-const Resource: React.FC<ResourceProps> = ({ selectedChapter, resources }) => {
-  console.log(selectedChapter);
+const Resource: React.FC<ResourceProps> = ({ selectedChapter, resources, selectedResource }) => {
   const [chapter, setChapter] = useState<IResource>([] as unknown as IResource);
-  const {
+  const [deleteResource, setDeleteResource] = useState(false);
+  const [editResource, setEditResource] = useState(false);
+  let {
     data = [],
     isSuccess,
     isLoading,
   } = useGetResourceByIdQuery(selectedChapter);
+
+  let [
+    deleteTopic,
+    {
+      isError: isDeleteError,
+      isSuccess: isdeleteSuccess,
+      error: deleteError,
+      isLoading: isdeleteloading,
+    },
+  ] =  useDeleteResourceMutation();
+
+  const handleDeleteTopic = async () => {
+    deleteTopic(selectedChapter);
+  };
+
+  useEffect(() => {
+    if (isdeleteSuccess) {
+      setDeleteResource(false);
+      setChapter([] as unknown as IResource);
+    }
+  }
+  , [isdeleteSuccess, deleteError, isDeleteError]);
 
   useEffect(() => {
     if (isSuccess && data.value) {
@@ -28,11 +57,12 @@ const Resource: React.FC<ResourceProps> = ({ selectedChapter, resources }) => {
     return <div>Loading...</div>;
   }
   return (
-    <div className="w-full">
+    <div className="flex gap-3 w-full">
+    <div className=" max-w-4xl mt-5">
       {chapter ? (
         <div className="flex-col items-center justify-center w-full">
           <h1 className="text-2xl font-bold mb-4">{chapter.title}</h1>
-          <p>{chapter.content}</p>
+          <div dangerouslySetInnerHTML={createMarkup(chapter.content)} />
         </div>
       ) : (
         <div>
@@ -40,6 +70,61 @@ const Resource: React.FC<ResourceProps> = ({ selectedChapter, resources }) => {
           <p>{resources[0].resources[0].title}</p>
         </div>
       )}
+    </div>
+    {editResource && (
+          <Modal onClose={() => setEditResource(false)}>
+            <EditResources
+              onClose={() => setEditResource(false)}
+              resource={{ id:selectedChapter, title: chapter.title, content: chapter.content, topicId: selectedResource }}
+            />
+          </Modal>
+        )}
+        {
+          deleteResource && (
+            <Modal onClose={() => setDeleteResource(false)}>
+              <div className="flex flex-col items-center justify-center gap-4">
+                <p className="text-lg font-bold">Are you sure?</p>
+                <div className="flex gap-4">
+                  <button
+                    onClick={() => setDeleteResource(false)}
+                    className="bg-primary text-white rounded-md px-4 py-2"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    onClick={handleDeleteTopic}
+                    className="bg-primary text-white rounded-md px-4 py-2"
+                  >
+                    {isdeleteloading ? (
+                <ProgressIndicator size={5} color="white" />
+              ) : (
+                "Delete"
+              )}
+                  </button>
+
+                </div>
+              </div>
+            </Modal>
+          )
+
+        }
+    {selectedChapter !== 0 && (<div className="flex flex-col w-full items-end gap-2 mt-5 mr-10">
+      <div>
+        <button onClick={() => setEditResource(true)}
+                    className="flex items-center rounded-full justify-center p-2 mt-5 bg-primary text-white"
+                  >
+          <AiFillEdit className="text-white" />
+        </button>
+      </div>
+      <div>
+        <button onClick={() => setDeleteResource(true)}
+                    className="flex items-center rounded-full justify-center p-2 mt-5 bg-black text-blue-500"
+                  >
+
+        <AiFillDelete className="text-white" />
+        </button>
+      </div>
+      </div>)}
     </div>
   );
 };
