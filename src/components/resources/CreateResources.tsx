d7793577b1a1cce32fc02c74b922b7f036946f4f
Zerihun Moges <zerihunmoges78@gmail.com>
2023-06-28 01:57:55 -0700

    add create resource

---
+++ src/components/resources/CreateResources.tsx
@@ -0,0 +1,134 @@
+import React, { useEffect, useState } from "react";
+import InputField from "../auth/InputField";
+import Button from "../common/Button";
+import { useCreateAnnouncementMutation } from "<@>/store/announcement/announcement-api";
+import ProgressIndicator from "../common/ProgressIndicator";
+import { AiOutlineCheck } from "react-icons/ai";
+import Editor from "../common/TextEditor";
+
+interface CreateAnnouncementProps {
+  onClose: () => void;
+}
+
+const CreateResources: React.FC<CreateAnnouncementProps> = ({ onClose }) => {
+  const [announcement, setAnnouncement] = useState({
+    title: "",
+    description: "",
+  });
+  const [announcementError, setAnnouncementError] = useState({
+    title: "",
+    description: "",
+  });
+  const handleChange = (e: any) => {
+    setAnnouncement({ ...announcement, [e.target.name]: e.target.value });
+  };
+
+  let [createAnnouncement, { data, isSuccess, isError, isLoading, error }] =
+    useCreateAnnouncementMutation();
+
+  const createError = error as any;
+  const validAnnouncement = () => {
+    if (announcement.title != "" && announcement.description != "") {
+      setAnnouncementError({
+        title: "",
+        description: "",
+      });
+      createAnnouncement(announcement);
+    } else
+      setAnnouncementError({
+        title: announcement.title ? "" : "Please insert Announcement title",
+        description: announcement.description
+          ? ""
+          : "please add announcement description",
+      });
+  };
+
+  useEffect(() => {
+    let intervalId: NodeJS.Timeout | null = null;
+
+    if (isSuccess) {
+      intervalId = setInterval(onClose, 1000);
+    }
+    return () => {
+      if (intervalId) clearInterval(intervalId);
+    };
+  }, [isSuccess]);
+
+  return (
+    <div className="w-full h-full p-2 flex flex-col gap-2">
+      <p className="font-bold text-lg">Create new Announcement</p>
+      <p className="text-sm opacity-30">
+        Add new Announcement to the system and easily get everyone up to speed.
+      </p>
+
+      <div>
+        {error &&
+          createError.data?.error?.map((err: any, index: number) => {
+            return <p className="text-xs text-red-500">{err.errorMessage}</p>;
+          })}{" "}
+        {error && !createError.data && (
+          <p className="text-xs text-red-500">Unknown Error</p>
+        )}
+      </div>
+
+      <form className="flex flex-col gap-2">
+        <InputField
+          label=""
+          name="title"
+          placeholder="Title"
+          type="text"
+          value={announcement.title}
+          onChange={handleChange}
+        />
+        <p className="text-xs text-red-500">
+          {announcementError.title !== "" && announcementError.title}
+        </p>
+
+        <Editor
+          value={announcement.description}
+          setValue={(value: string) =>
+            setAnnouncement({ ...announcement, description: value })
+          }
+        />
+        <p className="text-xs text-red-500">
+          {announcementError.description !== "" &&
+            announcementError.description}
+        </p>
+      </form>
+
+      <div className="flex justify-end gap-2">
+        <Button
+          onClick={() => onClose()}
+          className=" bg-secondary text-gray-800 font-medium "
+          label="Cancel"
+        ></Button>
+
+        {isLoading ? (
+          <Button
+            startIcon={<ProgressIndicator size={5}></ProgressIndicator>}
+            label=""
+          ></Button>
+        ) : isError ? (
+          <Button
+            onClick={() => validAnnouncement()}
+            className="font-medium"
+            label="Retry"
+          ></Button>
+        ) : isSuccess ? (
+          <Button
+            startIcon={<AiOutlineCheck></AiOutlineCheck>}
+            label=""
+          ></Button>
+        ) : (
+          <Button
+            onClick={() => validAnnouncement()}
+            className="font-medium"
+            label="Create"
+          ></Button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CreateResources;
---
+++ src/pages/admin/resources.tsx
@@ -0,0 +1,130 @@
+import AnnouncementCard from "<@>/components/admin/AnnouncementCard";
+import AnnouncementDetail from "<@>/components/admin/AnnouncementDetail";
+import CreateAnnouncement from "<@>/components/admin/CreateAnnouncement";
+import EditAnnouncement from "<@>/components/admin/EditAnnouncement";
+import Button from "<@>/components/common/Button";
+import Error from "<@>/components/common/Error";
+import Modal from "<@>/components/common/Modal";
+import { useGetAnnouncementsQuery } from "<@>/store/announcement/announcement-api";
+import { Announcement } from "<@>/types/admin/Announcement";
+import React, { useState } from "react";
+import { AiOutlinePlus } from "react-icons/ai";
+
+const index: React.FC = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [editAnnouncement, setEditAnnouncement] = useState(false);
+  const [createAnnouncement, setCreateAnnouncement] = useState(false);
+  const [announcementDetail, setAnnouncementDetail] = useState<number | null>();
+  const {
+    data: response,
+    isSuccess,
+    isLoading,
+    isError,
+  } = useGetAnnouncementsQuery({});
+
+  const announcements: Announcement[] = response?.value;
+
+  return (
+    <div className="w-full p-8">
+      {createAnnouncement && (
+        <Modal onClose={() => setCreateAnnouncement(false)}>
+          <CreateAnnouncement onClose={() => setCreateAnnouncement(false)} />
+        </Modal>
+      )}
+
+      {typeof announcementDetail === "number" && (
+        <Modal
+          onClose={() => {
+            setEditAnnouncement(false);
+
+            setAnnouncementDetail(null);
+          }}
+        >
+          {editAnnouncement ? (
+            <EditAnnouncement
+              announcement={
+                announcements.find(
+                  (announcement) => announcement.id === announcementDetail
+                )!
+              }
+              onClose={() => setEditAnnouncement(false)}
+            />
+          ) : (
+            <AnnouncementDetail
+              onClose={() => setAnnouncementDetail(null)}
+              onEdit={() => {
+                setEditAnnouncement(true);
+              }}
+              announcement={
+                announcements.find(
+                  (announcement) => announcement.id === announcementDetail
+                )!
+              }
+            />
+          )}
+        </Modal>
+      )}
+
+      <div className="flex justify-between w-full">
+        <p className="opacity-60 text-lg">Announcements</p>
+
+        <Button
+          onClick={() => {
+            setCreateAnnouncement(true);
+            setShowModal(true);
+          }}
+          startIcon={<AiOutlinePlus></AiOutlinePlus>}
+          label="New Announcements"
+        />
+      </div>
+
+      <div className="w-full grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 gap-8 p-4">
+        {isLoading ? (
+          Array.from({ length: 16 }).map((_, index) => (
+            <div className="animate-pulse flex flex-col gap-6 rounded-md shadow-md p-4 w-full">
+              <div className="flex flex-col gap-2">
+                <div className="w-[90%] rounded-sm h-4 bg-slate-200"></div>
+                <div className="w-[45%] rounded-sm h-4 bg-slate-200"></div>
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <div className="w-[90%] rounded-sm h-4 bg-slate-200"></div>
+                <div className="w-[90%] rounded-sm h-4 bg-slate-200"></div>
+                <div className="w-[90%] rounded-sm h-4 bg-slate-200"></div>
+                <div className="w-[90%] rounded-sm h-4 bg-slate-200"></div>
+              </div>
+              <div className="self-end w-24 rounded-sm h-4 bg-slate-200"></div>
+            </div>
+          ))
+        ) : isSuccess ? (
+          response?.error ? (
+            <Error message={"An error occured while fetching Contests"} />
+          ) : (
+            announcements.map((announcement, index) => {
+              return (
+                <AnnouncementCard
+                  key={index}
+                  onClick={() => {
+                    setAnnouncementDetail(announcement.id);
+                    setShowModal(true);
+                  }}
+                  title={announcement.title}
+                  description={announcement.description}
+                  date={announcement.dateCreated!}
+                />
+              );
+            })
+          )
+        ) : isError ? (
+          <div>
+            <Error message={"An error occured while fetching Contests"} />
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default index;
