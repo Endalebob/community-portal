c43f15385ce8d63b0f6cd61d11a3432e0619b197
Bruk Tedla <biruk.tedla93@gmail.com>
2023-06-28 17:11:23 +0300

    fix(frontend): fix contests

--- src/components/contest/ContestList.tsx
+++ src/components/contest/ContestList.tsx
@@ -57,122 +57,118 @@ const ContestList: React.FC = () => {
   }
   const contestData = contests.value;
   return (
-    <div>
-      <div className="mt-8 sm:ml-2">
-        <h1 className="text-secondary-text font-semibold ml-6 text-lg">
-          OverView
-        </h1>
-        <OverViewContests />
-      </div>
-      <div className="relative overflow-x-auto shadow-md sm:rounded-lg pl-4 pr-8">
-        {role === "HeadOfEducation" && (
-          <div className="grid grid-cols-2 mt-8">
+    <div className="flex flex-col px-8 items-center">
+      <div className="max-w-7xl w-full">
+        <div className="mt-8">
+          <h1 className="text-zinc-400 font-medium text-2xl">Overview</h1>
+          <OverViewContests />
+        </div>
+        <div className="relative overflow-x-auto sm:rounded-lg">
+          {role === "HeadOfEducation" && (
+            <div className="grid grid-cols-2 mt-8">
+              <div>
+                <h1 className="text-zinc-400 font-medium text-2xl">Contests</h1>
+              </div>
+              <div className="pb-4 col-span-1 justify-self-end">
+                <button
+                  className="px-4 py-1 bg-primary text-white rounded-md justify-self-end"
+                  onClick={() => router.push("/contests/create-contest")}
+                >
+                  <span className="font-bold">+</span> New Contest
+                </button>
+              </div>
+            </div>
+          )}
+          {contestData.length === 0 ? (
+            <div className="max-auto text-center font-bold mb-4">
+              No contests added yet. Check back soon for updates!
+            </div>
+          ) : (
             <div>
-              <h1 className="text-secondary-text font-semibold ml-2 text-lg col-span-1">
-                Contests
-              </h1>
-            </div>
-            <div className="pb-4 col-span-1 justify-self-end">
-              <button
-                className="px-4 py-1 bg-primary text-white rounded-md justify-self-end"
-                onClick={() => router.push("/contests/create-contest")}
-              >
-                <span className="font-bold">+</span> New Contest
-              </button>
-            </div>
-          </div>
-        )}
-        {contestData.length === 0 ? (
-          <div className="max-auto text-center font-bold mb-4">
-            No contests added yet. Check back soon for updates!
-          </div>
-        ) : (
-          <div>
-            {role !== "HeadOfEducation" && (
-              <h1 className="text-secondary-text font-semibold mt-8 mb-4 text-lg">
-                Contests
-              </h1>
-            )}
-            <table className="w-full text-sm text-left text-gray-500">
-              <thead className="text-xs text-gray-700 bg-gray-50">
-                <tr>
-                  <th scope="col" className="px-6 py-4">
-                    Title
-                  </th>
-                  <th scope="col" className="px-6 py-4">
-                    Date
-                  </th>
-                  <th scope="col" className="px-6 py-4">
-                    Time
-                  </th>
-                  <th scope="col" className="px-6 py-4">
-                    Codeforces
-                  </th>
-                  {role === "HeadOfEducation" && (
+              {role !== "HeadOfEducation" && (
+                <h1 className="text-zinc-400 font-medium text-2xl">Contests</h1>
+              )}
+              <table className="w-full text-sm text-left text-gray-500">
+                <thead className="text-xs text-gray-700 bg-gray-50">
+                  <tr>
                     <th scope="col" className="px-6 py-4">
-                      Actions
+                      Title
                     </th>
-                  )}
-                </tr>
-              </thead>
-              <tbody>
-                {contestData.map((contest: any, index: any) => (
-                  <tr
-                    key={index}
-                    className={`border-b ${
-                      index % 2 ? "bg-gray-50" : "bg-white"
-                    }`}
-                  >
-                    <td
-                      scope="row"
-                      className="px-6 py-5 font-medium text-gray-900 whitespace-nowrap"
-                    >
-                      {contest.title}
-                    </td>
-                    <td className="px-6 py-4 font-medium text-gray-900">
-                      {contest.date.split("T")[0]}
-                    </td>
-                    <td className="px-6 py-4 font-medium text-gray-900">
-                      {contest.date.split("T")[1]}
-                    </td>
-                    <td className="px-6 py-4 font-medium text-primary underline">
-                      <a href={contest.link}>Link</a>
-                    </td>
+                    <th scope="col" className="px-6 py-4">
+                      Date
+                    </th>
+                    <th scope="col" className="px-6 py-4">
+                      Time
+                    </th>
+                    <th scope="col" className="px-6 py-4">
+                      Codeforces
+                    </th>
                     {role === "HeadOfEducation" && (
-                      <td className="flex flex-row py-4 font-medium my-auto flex-wrap">
-                        <div></div>
-                        <div className="mx-auto my-auto mt-4 md:mt-0 text-center">
-                          <a
-                            href={`/contests/${contest.id}`}
-                            className="text-primary hover:underline"
-                          >
-                            Edit
-                          </a>
-                        </div>
-
-                        <button
-                          onClick={() => handleDelete(contest.id)}
-                          className="text-red-600 hover:underline px-4"
-                        >
-                          Remove
-                        </button>
-
-                        <div className="mx-auto my-auto mt-4 md:mt-0 text-center">
-                          <Link
-                            className="text-primary hover:underline"
-                            href={`/admin/contest/stats/${contest.id}`}
-                          >
-                            Stats
-                          </Link>
-                        </div>
-                      </td>
+                      <th scope="col" className="px-6 py-4">
+                        Actions
+                      </th>
                     )}
                   </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        )}
+                </thead>
+                <tbody>
+                  {contestData.map((contest: any, index: any) => (
+                    <tr
+                      key={index}
+                      className={`border-b ${
+                        index % 2 ? "bg-gray-50" : "bg-white"
+                      }`}
+                    >
+                      <td
+                        scope="row"
+                        className="px-6 py-5 font-medium text-gray-900 whitespace-nowrap"
+                      >
+                        {contest.title}
+                      </td>
+                      <td className="px-6 py-4 font-medium text-gray-900">
+                        {contest.date.split("T")[0]}
+                      </td>
+                      <td className="px-6 py-4 font-medium text-gray-900">
+                        {contest.date.split("T")[1]}
+                      </td>
+                      <td className="px-6 py-4 font-medium text-primary">
+                        <a href={contest.link}>Link</a>
+                      </td>
+                      {role === "HeadOfEducation" && (
+                        <td className="flex flex-row py-4 font-medium my-auto flex-wrap">
+                          <div></div>
+                          <div className="mx-auto my-auto mt-4 md:mt-0 text-center">
+                            <a
+                              href={`/contests/${contest.id}`}
+                              className="text-primary hover:underline"
+                            >
+                              Edit
+                            </a>
+                          </div>
+
+                          <button
+                            onClick={() => handleDelete(contest.id)}
+                            className="text-red-600 hover:underline px-4"
+                          >
+                            Remove
+                          </button>
+
+                          <div className="mx-auto my-auto mt-4 md:mt-0 text-center">
+                            <Link
+                              className="text-primary hover:underline"
+                              href={`/admin/contest/stats/${contest.id}`}
+                            >
+                              Stats
+                            </Link>
+                          </div>
+                        </td>
+                      )}
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
