97d1a967f5bf2f0b8fdc0744118dbf85803ebcb1
Bruk Tedla <biruk.tedla93@gmail.com>
2023-06-28 16:51:51 +0300

    fix(frontend): add home link on logo

--- src/components/auth/AuthImage.tsx
+++ src/components/auth/AuthImage.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const AuthImage = () => {
@@ -6,13 +7,16 @@ const AuthImage = () => {
     <div className="hidden md:flex h-screen w-1/2 bg-cover bg-center relative  bg-gray-500 bg-[url('/images/a2Sv.png')] bg-blend-multiply">
       <div className="absolute inset-0 justify-center">
         <div className="mx-5 pt-5 flex flex-col h-full justify-between">
-          <Image
-            width={100}
-            height={100}
-            src="/images/logo.svg"
-            alt="logo"
-            className="w-32"
-          />
+          <Link href="/">
+            <Image
+              width={100}
+              height={100}
+              src="/images/logo.svg"
+              alt="logo"
+              className="w-32"
+            />
+          </Link>
+
           <div className="flex flex-col">
             <h1 className="text-4xl max-w-[439px] text-white font-bold font-Inter mt-5">
               Start making your dreams come true
