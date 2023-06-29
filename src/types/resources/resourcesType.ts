aa17b4053e52ddfa667c6c188fde979e94febaff
Brook Alemu <brookwworku3@gmail.com>
2023-06-28 21:31:27 -0900

    fix merge conflict on resource type and related

--- src/types/resources/resourcesType.ts
+++ src/types/resources/resourcesType.ts
@@ -2,9 +2,5 @@ export default interface IResource {
   id: number;
   title: string;
   content: string;
-<<<<<<< HEAD
   topicId?: number;
-=======
-  topicId?: string;
->>>>>>> a4c3b35 (set the limit for the length of title)
 }
