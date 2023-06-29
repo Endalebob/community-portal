766f932b2fe494705cec8bfa188a565338313be9
Zerihun Moges <zerihunmoges78@gmail.com>
2023-06-29 00:15:19 -0700

    add skeleton

--- src/components/resources/Resource.tsx
+++ src/components/resources/Resource.tsx
@@ -1,4 +1,8 @@
-import { useDeleteResourceMutation, useDeleteTopicMutation, useGetResourceByIdQuery } from "<@>/store/resource/resource-api";
+import {
+  useDeleteResourceMutation,
+  useDeleteTopicMutation,
+  useGetResourceByIdQuery,
+} from "<@>/store/resource/resource-api";
 import IResourceTopic from "<@>/types/resources/resourceListType";
 import IResource from "<@>/types/resources/resourcesType";
 import React, { use, useEffect, useState } from "react";
@@ -7,6 +11,7 @@ import { AiFillDelete, AiFillEdit } from "react-icons/ai";
 import EditResources from "./EditResource";
 import Modal from "../common/Modal";
 import ProgressIndicator from "../common/ProgressIndicator";
+import Error from "../common/Error";
 
 interface ResourceProps {
   selectedChapter: number;
@@ -14,14 +19,21 @@ interface ResourceProps {
   resources: IResourceTopic[];
 }
 
-const Resource: React.FC<ResourceProps> = ({ selectedChapter, resources, selectedResource }) => {
+const Resource: React.FC<ResourceProps> = ({
+  selectedChapter,
+  resources,
+  selectedResource,
+}) => {
   const [chapter, setChapter] = useState<IResource>([] as unknown as IResource);
   const [deleteResource, setDeleteResource] = useState(false);
   const [editResource, setEditResource] = useState(false);
   let {
+    currentData,
+    isFetching,
     data = [],
     isSuccess,
     isLoading,
+    isError,
   } = useGetResourceByIdQuery(selectedChapter);
 
   let [
@@ -32,7 +44,7 @@ const Resource: React.FC<ResourceProps> = ({ selectedChapter, resources, selecte
       error: deleteError,
       isLoading: isdeleteloading,
     },
-  ] =  useDeleteResourceMutation();
+  ] = useDeleteResourceMutation();
 
   const handleDeleteTopic = async () => {
     deleteTopic(selectedChapter);
@@ -43,8 +55,7 @@ const Resource: React.FC<ResourceProps> = ({ selectedChapter, resources, selecte
       setDeleteResource(false);
       setChapter([] as unknown as IResource);
     }
-  }
-  , [isdeleteSuccess, deleteError, isDeleteError]);
+  }, [isdeleteSuccess, deleteError, isDeleteError]);
 
   useEffect(() => {
     if (isSuccess && data.value) {
@@ -53,78 +64,115 @@ const Resource: React.FC<ResourceProps> = ({ selectedChapter, resources, selecte
     }
   }, [data, isSuccess]);
 
-  if (isLoading) {
-    return <div>Loading...</div>;
+  if (!currentData && isFetching) {
+    return (
+      <div className="w-full p-4  animate-pulse flex flex-col gap-16">
+        <div className="flex flex-col w-full gap-2">
+          <div className="w-[80%] h-4 bg-slate-200 rounded-sm"></div>
+          <div className="w-[30%] h-4 bg-slate-200 rounded-sm"></div>
+        </div>
+
+        <div className=" flex flex-col w-full gap-12">
+          <div className="flex w-full flex-col gap-4 rounded">
+            <div className="h-4 bg-slate-200 w-1/4 "></div>
+            <div className="h-4 bg-slate-200 w-1/2"></div>
+            <div className="h-4 bg-slate-200 w-3/4"></div>
+            <div className="h-4 bg-slate-200 w-1/3"></div>
+            <div className="h-4 bg-slate-200 w-3/4"></div>
+            <div className="h-4 bg-slate-200 w-1/2"></div>
+            <div className="h-4 bg-slate-200 w-1/4"></div>
+          </div>
+          <div className="w-1/3 h-56 rounded-md bg-slate-200"></div>
+          <div className="flex w-full flex-col gap-4 rounded">
+            <div className="h-4 bg-slate-200 w-1/4 "></div>
+            <div className="h-4 bg-slate-200 w-1/2"></div>
+            <div className="h-4 bg-slate-200 w-3/4"></div>
+            <div className="h-4 bg-slate-200 w-3/4"></div>
+            <div className="h-4 bg-slate-200 w-3/4"></div>
+            <div className="h-4 bg-slate-200 w-1/2"></div>
+            <div className="h-4 bg-slate-200 w-1/4"></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return <Error message="Error occurred while fetching content" />;
   }
   return (
     <div className="flex gap-3 w-full">
-    <div className=" max-w-4xl mt-5">
-      {chapter ? (
-        <div className="flex-col items-center justify-center w-full">
-          <h1 className="text-2xl font-bold mb-4">{chapter.title}</h1>
-          <div dangerouslySetInnerHTML={createMarkup(chapter.content)} />
-        </div>
-      ) : (
-        <div>
-          <h1 className="text-2xl font-bold mb-4">{resources[0].title}</h1>
-          <p>{resources[0].resources[0].title}</p>
+      <div className=" max-w-4xl mt-5">
+        {chapter ? (
+          <div className="flex-col items-center justify-center w-full">
+            <h1 className="text-2xl font-bold mb-4">{chapter.title}</h1>
+            <div dangerouslySetInnerHTML={createMarkup(chapter.content)} />
+          </div>
+        ) : (
+          <div>
+            <h1 className="text-2xl font-bold mb-4">{resources[0].title}</h1>
+            <p>{resources[0].resources[0].title}</p>
+          </div>
+        )}
+      </div>
+      {editResource && (
+        <Modal onClose={() => setEditResource(false)}>
+          <EditResources
+            onClose={() => setEditResource(false)}
+            resource={{
+              id: selectedChapter,
+              title: chapter.title,
+              content: chapter.content,
+              topicId: selectedResource,
+            }}
+          />
+        </Modal>
+      )}
+      {deleteResource && (
+        <Modal onClose={() => setDeleteResource(false)}>
+          <div className="flex flex-col items-center justify-center gap-4">
+            <p className="text-lg font-bold">Are you sure?</p>
+            <div className="flex gap-4">
+              <button
+                onClick={() => setDeleteResource(false)}
+                className="bg-primary text-white rounded-md px-4 py-2"
+              >
+                Cancel
+              </button>
+              <button
+                onClick={handleDeleteTopic}
+                className="bg-primary text-white rounded-md px-4 py-2"
+              >
+                {isdeleteloading ? (
+                  <ProgressIndicator size={5} color="white" />
+                ) : (
+                  "Delete"
+                )}
+              </button>
+            </div>
+          </div>
+        </Modal>
+      )}
+      {selectedChapter !== 0 && (
+        <div className="flex flex-col w-full items-end gap-2 mt-5 mr-10">
+          <div>
+            <button
+              onClick={() => setEditResource(true)}
+              className="flex items-center rounded-full justify-center p-2 mt-5 bg-primary text-white"
+            >
+              <AiFillEdit className="text-white" />
+            </button>
+          </div>
+          <div>
+            <button
+              onClick={() => setDeleteResource(true)}
+              className="flex items-center rounded-full justify-center p-2 mt-5 bg-black text-blue-500"
+            >
+              <AiFillDelete className="text-white" />
+            </button>
+          </div>
         </div>
       )}
-    </div>
-    {editResource && (
-          <Modal onClose={() => setEditResource(false)}>
-            <EditResources
-              onClose={() => setEditResource(false)}
-              resource={{ id:selectedChapter, title: chapter.title, content: chapter.content, topicId: selectedResource }}
-            />
-          </Modal>
-        )}
-        {
-          deleteResource && (
-            <Modal onClose={() => setDeleteResource(false)}>
-              <div className="flex flex-col items-center justify-center gap-4">
-                <p className="text-lg font-bold">Are you sure?</p>
-                <div className="flex gap-4">
-                  <button
-                    onClick={() => setDeleteResource(false)}
-                    className="bg-primary text-white rounded-md px-4 py-2"
-                  >
-                    Cancel
-                  </button>
-                  <button
-                    onClick={handleDeleteTopic}
-                    className="bg-primary text-white rounded-md px-4 py-2"
-                  >
-                    {isdeleteloading ? (
-                <ProgressIndicator size={5} color="white" />
-              ) : (
-                "Delete"
-              )}
-                  </button>
-
-                </div>
-              </div>
-            </Modal>
-          )
-
-        }
-    {selectedChapter !== 0 && (<div className="flex flex-col w-full items-end gap-2 mt-5 mr-10">
-      <div>
-        <button onClick={() => setEditResource(true)}
-                    className="flex items-center rounded-full justify-center p-2 mt-5 bg-primary text-white"
-                  >
-          <AiFillEdit className="text-white" />
-        </button>
-      </div>
-      <div>
-        <button onClick={() => setDeleteResource(true)}
-                    className="flex items-center rounded-full justify-center p-2 mt-5 bg-black text-blue-500"
-                  >
-
-        <AiFillDelete className="text-white" />
-        </button>
-      </div>
-      </div>)}
     </div>
   );
 };
--- src/components/resources/Sidebar.tsx
+++ src/components/resources/Sidebar.tsx
@@ -10,9 +10,10 @@ import Modal from "../common/Modal";
 import CreateResources from "./CreateResource";
 import EditResources from "./EditResource";
 import CreateTopic from "./CreateTopic";
+import Error from "../common/Error";
 
 const SideBar: React.FC = () => {
-  const [selectedResource, setSelectedResource] = useState<number>(0);
+  const [selectedResource, setSelectedResource] = useState<number>();
   const [expand, setExpand] = useState<boolean>(false);
   const [selectedChapter, setSelectedChapter] = useState<number>(0);
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
@@ -28,10 +29,6 @@ const SideBar: React.FC = () => {
     }
   }, [data, isSuccess]);
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
   const handleChapterClick = (resourceId: number, chapterId: number) => {
     setSelectedResource(resourceId);
     setSelectedChapter(chapterId);
@@ -40,12 +37,10 @@ const SideBar: React.FC = () => {
   const handleResourceClick = (resourceId: number) => {
     if (selectedResource !== resourceId) {
       setExpand(true);
-    }
-    else{
+    } else {
       setExpand(!expand);
     }
     setSelectedResource(resourceId);
-    setSelectedChapter(0);
   };
   const handleToggleMenu = () => {
     setIsSidebarOpen(!isSidebarOpen);
@@ -67,7 +62,7 @@ const SideBar: React.FC = () => {
           <Modal onClose={() => setCreateResouce(false)}>
             <CreateResources
               onClose={() => setCreateResouce(false)}
-              topicId={selectedResource}
+              topicId={selectedResource!}
             />
           </Modal>
         )}
@@ -84,74 +79,158 @@ const SideBar: React.FC = () => {
             <MdMenu />
           </button>
         </div>
-        <div className="flex flex-col">
-          {resources.map((resource) => (
-            <div key={resource.id} className="flex flex-col">
-              <div
-                onClick={() => handleResourceClick(resource.id)}
-                className={`flex items-cent rounded-md justify-between px-4 py-2 cursor-pointer ${
-                  selectedResource === resource.id
-                    ? "text-primary"
-                    : "text-black"
-                }`}
-              >
-                <h1 className="font-bold  w-full">
-                  {resource.title.length > 30
-                    ? resource.title.substring(0, 30) + "..."
-                    : resource.title}
-                </h1>
-              </div>
-              {expand && (selectedResource === resource.id) &&
-                resource.resources.map((chapter) => (
-                  <div className="flex items-center ml-10">
-                    <p>
-                      <BsFillJournalBookmarkFill  />
-                    </p>
-                    
-                    <div
-                      key={chapter.id}
-                      onClick={() =>
-                        handleChapterClick(resource.id, chapter.id)
-                      }
-                      className={`flex items-center justify-between pl-2 pr-4 py-2 cursor-pointer ${
-                        selectedChapter === chapter.id
-                          ? "text-primary"
-                          : "text-gray-700"
-                      }`}
-                    >
-                      <h1 className="">
-                        {chapter.title.length > 25
-                          ? chapter.title.substring(0, 25) + "..."
-                          : chapter.title}
-                      </h1>
-                    </div>
-                  </div>
-                ))}
-              {expand && (selectedResource === resource.id) && (
-                <div className="flex items-center ml-10">
-                  <button
-                    onClick={() => setCreateResouce(true)}
-                    className="flex items-center rounded-full justify-center p-2 mt-5 bg-primary text-white"
-                  >
-                    <MdAdd className=" text-white" />
-                  </button>
-                </div>
-              )}
+
+        {isLoading ? (
+          <div className="flex flex-col animate-pulse p-4 gap-16">
+            <div className="flex flex-col gap-4">
+              <div className="w-[60%] h-4 bg-gray-200 rounded-sm"></div>
+              <div className="w-[80%] h-4 bg-gray-200 rounded-sm"></div>
+              <div className="w-[50%] h-4 bg-gray-200 rounded-sm"></div>
+              <div className="w-[100%] h-4 bg-gray-200 rounded-sm"></div>
+              <div className="w-[80%] h-4 bg-gray-200 rounded-sm"></div>
+              <div className="w-[50%] h-4 bg-gray-200 rounded-sm"></div>
             </div>
-          ))}
-        </div>
-        <div className="ml-5 my-20">
-          <button
-            onClick={() => setCreateTopic(true)}
-            className="flex items-center justify-center px-4 py-2 bg-primary text-white rounded-md"
-          >
-            <MdAdd className="mr-2" />
-            Add Topic
-          </button>
-        </div>
+
+            <div className="w-[50%] h-12 bg-gray-200 rounded-sm"></div>
+          </div>
+        ) : isSuccess ? (
+          <>
+            <div className="flex flex-col">
+              {resources.map((resource) => (
+                <div key={resource.id} className="flex flex-col">
+                  <div
+                    onClick={() => handleResourceClick(resource.id)}
+                    className={`flex items-cent rounded-md justify-between px-4 py-2 cursor-pointer ${
+                      selectedResource === resource.id
+                        ? "text-primary"
+                        : "text-black"
+                    }`}
+                  >
+                    <h1 className="font-bold  w-full">
+                      {resource.title.length > 30
+                        ? resource.title.substring(0, 30) + "..."
+                        : resource.title}
+                    </h1>
+                  </div>
+                  {expand &&
+                    selectedResource === resource.id &&
+                    resource.resources.map((chapter) => (
+                      <div className="flex items-center ml-10">
+                        <p>
+                          <BsFillJournalBookmarkFill />
+                        </p>
+
+                        <div
+                          key={chapter.id}
+                          onClick={() =>
+                            handleChapterClick(resource.id, chapter.id)
+                          }
+                          className={`flex items-center justify-between pl-2 pr-4 py-2 cursor-pointer ${
+                            selectedChapter === chapter.id
+                              ? "text-primary"
+                              : "text-gray-700"
+                          }`}
+                        >
+                          <h1 className="">
+                            {chapter.title.length > 25
+                              ? chapter.title.substring(0, 25) + "..."
+                              : chapter.title}
+                          </h1>
+                        </div>
+                      </div>
+                    ))}
+                  {expand && selectedResource === resource.id && (
+                    <div className="flex items-center ml-10">
+                      <button
+                        onClick={() => setCreateResouce(true)}
+                        className="flex items-center rounded-full justify-center p-2 mt-5 bg-primary text-white"
+                      >
+                        <MdAdd className=" text-white" />
+                      </button>
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+            <div className="ml-5 my-20">
+              <button
+                onClick={() => setCreateTopic(true)}
+                className="flex items-center justify-center px-4 py-2 bg-primary text-white rounded-md"
+              >
+                <MdAdd className="mr-2" />
+                Add Topic
+              </button>
+            </div>
+          </>
+        ) : (
+          <Error message="Error occurred while fetching resources" />
+        )}
       </div>
       <div className="flex flex-col w-full h-full">
-        <Resource selectedChapter={selectedChapter} resources={resources} selectedResource={selectedResource} />
+        {selectedChapter && selectedResource ? (
+          <Resource
+            selectedChapter={selectedChapter}
+            resources={resources}
+            selectedResource={selectedResource}
+          />
+        ) : (
+          <div className="max-w-3xl mx-auto py-8">
+            <h1 className="text-3xl font-bold mb-4">
+              Welcome to Our Resource Page
+            </h1>
+            <p className="text-lg text-gray-700 mb-8">
+              Designed to empower students like you with a wealth of knowledge
+              and tools to support your learning journey.
+            </p>
+
+            <p className="text-lg text-gray-700 mb-4">
+              Explore our extensive collection of resources, conveniently
+              categorized into different subjects, to find exactly what you
+              need. Whether you're seeking study guides, tutorials, practice
+              exercises, or informative articles, our resource page offers a
+              wide array of materials to cater to your specific learning
+              preferences.
+            </p>
+
+            <p className="text-lg text-gray-700 mb-4">
+              Each topic section is meticulously crafted to provide a rich and
+              engaging learning experience. From mathematics and science to
+              literature and history, our resources cover a broad spectrum of
+              academic disciplines. Delve into in-depth articles that explain
+              complex concepts, access interactive learning modules that bring
+              abstract ideas to life, or find recommended external resources to
+              expand your knowledge even further.
+            </p>
+
+            <p className="text-lg text-gray-700 mb-4">
+              We understand that different students have different learning
+              styles, which is why our resource page includes a diverse range of
+              content formats. Visual learners can benefit from infographics and
+              videos, while text-based resources are available for those who
+              prefer reading and textual explanations. Our goal is to
+              accommodate various learning preferences and ensure that every
+              student can find resources that resonate with their unique way of
+              absorbing information.
+            </p>
+
+            <p className="text-lg text-gray-700 mb-4">
+              Whether you're looking to reinforce your understanding of a
+              particular subject, preparing for exams, or simply expanding your
+              knowledge beyond the classroom, our resource page is your go-to
+              destination. We continually update and add new resources to keep
+              up with the evolving educational landscape, ensuring that you have
+              access to the latest and most relevant materials.
+            </p>
+
+            <p className="text-lg text-gray-700">
+              Visit our resource page and embark on an enriching learning
+              adventure. Empower yourself with the knowledge and tools needed to
+              excel academically and reach your full potential. The world of
+              learning awaits you, and our resource page is here to guide you
+              every step of the way.
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
