46d768e3ebbb1087027d5820491c1b9ef26b1054
aklile-yilma <aklileyilma@gmail.com>
2023-06-29 12:14:24 +0300

    feat(notificaitons): add notifications popup

---
+++ src/components/notifications/NotificationsCard.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { createMarkup } from "../common/TextEditor";
+import { FaRegCalendarAlt } from "react-icons/fa";
+
+interface NotifcationCardProps {
+  title: string;
+  content: string;
+  date: string;
+}
+const NotificationsCard = ({ title, content, date }: NotifcationCardProps) => {
+  return (
+    <div className={"bg-white rounded-lg shadow-lg p-6"}>
+      <h2 className="text-xl font-semibold mb-2 text-start">{title}</h2>
+      <p
+        className="text-start"
+        dangerouslySetInnerHTML={createMarkup(content)}
+      />
+
+      {/* <p className="text-gray-700 mb-4">{content}</p> */}
+      <div className="flex items-center text-gray-600">
+        <FaRegCalendarAlt className="mr-2" />
+        <span>{date}</span>
+      </div>
+    </div>
+  );
+};
+
+export default NotificationsCard;
